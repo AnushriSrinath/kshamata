4cb5da6fb599a5207af2d426b0b331f86b615bee
Vishnu\Spark <shreevishnu456@gmail.com>
2017-07-09 07:27:41 +0530

    Vishnu11

--- MyApplication2/Kshamata/app/src/main/java/com/example/anu/kshamata/Manager.java
+++ MyApplication2/Kshamata/app/src/main/java/com/example/anu/kshamata/Manager.java
@@ -2,10 +2,15 @@ package com.example.anu.kshamata;
 
 import android.app.Application;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Created by anu on 09-07-2017.
  */
 public class Manager extends Application {
-
+    static ArrayList<String> listofwomen = new ArrayList<String>(Arrays.asList("Anitha", "Mangala" , "Sunitha" , "Poorvi", "Soumya"));
+    static ArrayList<String> listofvolunteers = new ArrayList<String>(Arrays.asList("Balu", "Praveen" , "Manju" , "Vinay", "Varun"));
 
 }
